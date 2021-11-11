c4d008f78c6eadd1b30c0ebd649214495d0b8ec5
Memet Ekici <meban2009@gmail.com>
2021-11-11 19:20:40 +0100

    This is my local memet branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("local repodaki ilk commitimiz");
 		System.out.println("Our second commit from local repository");
+		System.out.println("Now I am on my memet branchs");
 		
 	}
 
