d145131e5b3f3a151285b9722184755f355c75dd
Memet Ekici <meban2009@gmail.com>
2021-11-11 19:06:55 +0100

    Our second commit from our git repository

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -3,6 +3,7 @@ package first_package;
 public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("local repodaki ilk commitimiz");
+		System.out.println("Our second commit from local repository");
 		
 	}
 
